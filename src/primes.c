192f0444e8c9c74afacadc740c82f822271f0848
Stanislav Kafara <skafara@students.zcu.cz>
2022-10-31 22:42:26 +0100

    add primes v1

---
+++ src/primes.c
@@ -0,0 +1,121 @@
+/**
+ * \file primes.c
+ * \brief Functions declared in primes.h are implemented in this file.
+ */
+
+
+#include <stdlib.h>
+#include <time.h>
+
+#include "primes.h"
+
+
+/** Test case 1: numbers < 2047 are tested against this test numbers subset. */
+const size_t test_a1[] = {2};
+/** Test case 2: numbers >= 2047 and < 1373653 are tested against this test numbers subset. */
+const size_t test_a2[] = {2, 3};
+
+
+/**
+ * \brief Performs actual testing for primality. 
+ *        If test_a is NULL, algorithm is probabilistic, else deterministic.
+ * \param n Number to be tested for primality.
+ * \param test_a Test numbers subset against which the provided number will be tested.
+ * \param test_cnt Number of tests the number must pass to be able to be classified as a prime number.
+ * \return 
+ */
+int mil_rab_test(size_t n, const size_t test_a[], size_t test_cnt) {
+    size_t nm;
+    size_t k, m;
+    size_t b, a;
+    size_t i, j;
+
+    nm = n - 1;
+
+    k = 0;
+    m = nm;
+    while (m % 2 == 0) {
+        k++;
+        m /= 2;
+    }
+
+    i = 0;
+    test_loop:
+    while (i < test_cnt) {
+        if (test_a) {
+            a = test_a[i];
+        }
+        else {
+            a = (rand() % (nm - 2 + 1)) + 2;
+        }
+
+        b = 1;
+        for (j = 0; j < m; j++) {
+            b = (b * a) % n;
+        }
+        if (b == 1 || b == nm) {
+            i++;
+            goto test_loop;
+        }
+
+        for (j = 0; j < k; j++) {
+            b = (b * b) % n;
+
+            if (b == 1) {
+                return 0;
+            }
+            if (b == nm) {
+                i++;
+                goto test_loop;
+            }
+        }
+
+        return 0;
+    }
+
+    return 1;
+}
+
+
+/**
+ * \brief mil_rab Evaluates how to test provided number and calls the actual testing.
+ *                Correctness of 100 % for numbers < 1373653.
+ *                Correctness of 99.99847412109375 % for numbers >= 1373653.
+ * \param num Number to be tested for primality (> 2).
+ * \return 1 if num is determined to be prime, 0 if num is definetely not prime.
+ */
+int mil_rab(size_t num) {
+    if (num < P_TEST_N1) {
+        return mil_rab_test(num, test_a1, P_TEST_A1_LEN);
+    }
+    else if (num < P_TEST_N2) {
+        return mil_rab_test(num, test_a2, P_TEST_A2_LEN);
+    }
+    else {
+        srand(time(NULL));
+        return mil_rab_test(num, NULL, P_TEST_R_CNT);
+    }
+}
+
+
+int is_prime(size_t num) {
+    if (num < 2) {
+        return 0;
+    }
+    if (num == 2) {
+        return 1;
+    }
+    if (num % 2 == 0) {
+        return 0;
+    }
+
+    return mil_rab(num);
+}
+
+
+size_t next_prime(size_t num) {
+    while (!is_prime(num)) {
+        num++;
+    }
+    return num;
+}
---
+++ src/primes.h
@@ -0,0 +1,51 @@
+/**
+ * \file primes.h
+ * \brief Header file related to number primality test and prime numbers generation.
+ *
+ * Used for testing of number primality and generating prime numbers.
+ * Operations are based on Miller-Rabin probabilistic algorithm.
+ * Operations are optimized and 100 % correct (not probabilistic) for numbers < 1373653.
+ * For numbers >= 1373653 the operations use the probabilistic approach
+ * with 99.99847412109375 % accuracy of correct classification.
+ */
+
+
+#include <stddef.h>
+
+
+#ifndef PRIMES_H
+#define PRIMES_H
+
+
+/**< Test case 1: numbers < 2047. */
+#define P_TEST_N1 2047
+/**< Test case 1: test subset length 1. */
+#define P_TEST_A1_LEN 1
+/**< Test case 2: numbers >= 2047 and < 1373653. */
+#define P_TEST_N2 1373653
+/**< Test case 2: test subset length 2. */
+#define P_TEST_A2_LEN 2
+/**< Test case 3: test subset length for numbers >= 1373653. */
+#define P_TEST_R_CNT 8
+
+
+/**
+ * \brief is_prime Determines whether a number is prime.
+ *                 Correctness of 100 % for numbers < 1373653.
+ *                 Correctness of 99.99847412109375 % for numbers >= 1373653.
+ * \param num Number to be tested for primality.
+ * \return 1 if num is determined to be prime, 0 if num is definetely not prime.
+ */
+int is_prime(size_t num);
+
+
+/**
+ * \brief next_prime Returns next prime number that is greater than or equal to provided number.
+ *                   Uses the is_prime function for primality testing.
+ * \param num Number used to find out the next prime number.
+ * \return Lowest prime number >= num.
+ */
+size_t next_prime(size_t num);
+
+
+#endif
