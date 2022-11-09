33ad6743854463d961e35bd39b896d9fb9b94073
Stanislav Kafara <skafara@students.zcu.cz>
2022-11-09 06:07:33 +0100

    add item value deallocator to hashtable

--- src/hashtable.h
+++ src/hashtable.h
@@ -21,6 +21,14 @@
 /**< Hashtable average items count per basket. */
 #define H_ITEMS_PER_BUCKET 5
 
+
+/**
+ * \brief Pointer to a function, which will perform
+ *        releasing the memory held by the value of a hashtable item.
+ */
+typedef void (*htab_item_value_deallocator)(void **value);
+
+
 /**
  * \struct htab_link
  * \brief Struct representing an entry (link) in a hashtable.
@@ -42,6 +50,8 @@ typedef struct htab_ {
     size_t buckets_cnt;             /**< Hashtable buckets count. */
     size_t items_cnt;               /**< Hashtable items count. */
     const size_t item_value_size;   /**< Hashtable item value size. */
+    const htab_item_value_deallocator item_value_deallocator;
+                                    /**< Hashtable item value deallocator. */
 } htab;
 
 
@@ -49,18 +59,20 @@ typedef struct htab_ {
  * \brief htab_create Creates an empty hashtable with default buckets count
  *                    ready to work with items with values of provided size.
  * \param item_value_size Size of hashtable item value.
+ * \param item_value_deallocator Pointer to a function that frees hashtable item values, when hashtable is freed.
  * \return Pointer to a new empty hashtable with aformentioned properties.
  */
-htab *htab_create(const size_t item_value_size);
+htab *htab_create(const size_t item_value_size, const htab_item_value_deallocator item_value_deallocator);
 
 
 /**
  * \brief htab_free Releases the memory held by the hashtable
  *                  - frees htab struct
  *                  -- frees each htab_link struct in buckets
- *                  --- frees htab_link key (char array copy)
- *                                      and value (void pointer to copy of the value,
-                                                   where either a pointer or a direct value is stored)
+ *                  --- frees htab_link item value, if htab_item_value_deallocator was provided
+ *                  ---- frees htab_link key (char array copy)
+ *                                       and value (copy of the value,
+ *                                                  where either a pointer or a direct value is stored)
  *                  and NULLs the pointer to the hashtable.
  * \param ht Pointer to a pointer to a hashtable.
  */
